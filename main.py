26f09c4c0eda1391f88394e77cf9dbcb1f79cff0
Chris Behan <behan@hey.com>
2021-02-14 22:12:42 +0000

    Adds login with twitter functionality.

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import request
+from flask import jsonify
 import tweet
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def index():
-  return "hello world!"
+    return "hello world!"
+
+
+@app.route('/login')
+def login_handler():
+    return tweet.login()
+
+
+@app.route('/getUser', methods=['POST'])
+def get_user_handler():
+    body = request.json
+    oauth_verifier = body['oauth_verifier']
+    oauth_token = body['oauth_token']
+    user = tweet.get_user(oauth_verifier, oauth_token)
+    return jsonify(**user.dict())
 
 @app.route('/tweet', methods=['POST'])
 def tweet_handler():
-  data = request.json
-  if 'text' not in data:
-    return "Expected 'text' to be in body of request", 400
+    data = request.json
+    if 'text' not in data:
+        return "Expected 'text' to be in body of request", 400
 
-  print(data)
-  tweet.tweet('hello')
-  return "received!"
-  
+    print(data)
+    tweet.tweet('hello')
+    return "received!"
+
+
+    
+
 app.run(host='0.0.0.0', port=8080)
--- tweet.py
+++ tweet.py
@@ -1,16 +1,70 @@
 import os
 from TwitterAPI import TwitterAPI
+import requests
+from urllib.parse import parse_qs
+from requests_oauthlib import OAuth1
+from user import User
+from typing import Dict
 
 consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token_key = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+user_api = TwitterAPI(consumer_key, consumer_secret, access_token_key,
+                      access_token_secret)
+
+callback_uri = 'https://tweetqueue.behan.repl.co/queue.html'
+request_key = None
+request_secret = None
 
 
-def tweet(text: str):
-  print(f'You tweeted: {text}')
+def login():
+    oauth_token, oauth_token_secret = get_oauth_tokens()
+    twitter_sign_in_link = generate_sign_in_link(oauth_token)
+    return twitter_sign_in_link, 200
 
-def test(text):
-  res = api.request('statuses/update', {'status': text})
-  print(res.status_code) 
+
+def get_oauth_tokens():
+    oauth = OAuth1(consumer_key, consumer_secret, callback_uri=callback_uri)
+    res = requests.post(url='https://api.twitter.com/oauth/request_token',
+                        auth=oauth)
+    if res.status_code != 200:
+        raise Exception('Unexpected response from /oauth/request_token')
+    content = res.content.decode('utf-8')
+    credentials = parse_qs(content)
+    callback_confirmed = credentials['oauth_callback_confirmed'][0]
+    if callback_confirmed != 'true':
+        raise Exception('Callback url is not confirmed!')
+    oauth_token = credentials['oauth_token'][0]
+    oauth_token_secret = credentials['oauth_token_secret'][0]
+    return oauth_token, oauth_token_secret
+
+
+def generate_sign_in_link(token: str):
+    sign_in_link = f'https://api.twitter.com/oauth/authenticate?oauth_token={token}'
+    return sign_in_link
+
+
+def get_user(verifier:str, oauth_token:str):
+    credentials = get_credentials(verifier, oauth_token)
+    access_token_key = credentials.get('oauth_token')
+    access_token_secret = credentials.get('oauth_token_secret')
+    user_id = credentials.get('user_id')
+    screen_name = credentials.get('screen_name')
+    return User(access_token_key, access_token_secret, user_id, screen_name)
+
+
+def get_credentials(verifier: str, oauth_token: str) -> Dict[str, str]:
+    oauth = OAuth1(consumer_key,
+                   consumer_secret,
+                   resource_owner_key=oauth_token,
+                   verifier=verifier)
+    
+    response = requests.post(url='https://api.twitter.com/oauth/access_token',
+                      auth=oauth)
+    # response is in bytes, decode to utf-8 string
+    content = response.content.decode('utf-8')
+    credentials = parse_qs(content)
+    # credentials dict is in form {str: List[str]} where List[str] has 1 element.
+    # Transform to {str: str}
+    return {key:value[0] for (key, value) in credentials.items()}
